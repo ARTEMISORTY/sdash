5eaa3417e037f5db5b001655158e9f4b4aab63a9
Plato <123819515+ARTEMISORTY@users.noreply.github.com>
2024-11-06 21:02:02 +0400

    Revert "adding next optimized images"
    This reverts commit 80ed8f1020effb1efa2b115c01c44beca097a011.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
-import withOptimizedImages from 'next-optimized-images';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withOptimizedImages({
-  output: 'export',
-  images: {
-    unoptimized: true,
-  }
-});
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export'
+  };
+  
+  export default nextConfig;
+  
